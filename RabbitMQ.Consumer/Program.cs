d5889baaf1f0cddc8b6ca015cdc23e626119ccc3
Onur Nafi Guzel <nafi.guzel@consultant.boyner.com.tr>
2024-05-02 00:49:32 +0300

    Basic RabbitMQ.Consumer

---
+++ RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,25 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+// Bağlantı oluşturma
+ConnectionFactory factory = new();
+factory.Uri = new Uri("amqps://mkckuyqi:T72UpsjX0A1wCShoziWPCM9mK5JgjpGx@woodpecker.rmq.cloudamqp.com/mkckuyqi");
+
+// Bağlantıyı aktifleştirme ve kanal açma
+using IConnection connection = factory.CreateConnection(); //IDisposable olduğu için using kullanıyorum
+using IModel channel = connection.CreateModel();
+
+// Queue oluşturma
+channel.QueueDeclare(queue: "example-queue", exclusive: false);
+
+// Queue'dan mesaj okuma
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: "example-queue", false, consumer);
+consumer.Received += (sender, e) =>
+{
+	// Kuyruğa gelen mesajın işlendiği yerdir!
+	// e.body : Kuyruktaki mesajın verisini bütünsel olarak getirecektir. -> e.body.span || e.body.toarray()
+	Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+};
+Console.Read();
