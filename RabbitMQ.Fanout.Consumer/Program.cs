1448fed4a26f8a6442e5c0b82ff81b6b52947254
Onur Nafi Guzel <nafi.guzel@consultant.boyner.com.tr>
2024-05-04 14:02:11 +0300

    7.Ders Fanout Exchange

---
+++ RabbitMQ.Fanout.Consumer/Program.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+
+factory.Uri = new Uri("amqps://mkckuyqi:T72UpsjX0A1wCShoziWPCM9mK5JgjpGx@woodpecker.rmq.cloudamqp.com/mkckuyqi");
+
+using IConnection connection = factory.CreateConnection(); 
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(
+	exchange: "fanout-exchange-example",
+	type: ExchangeType.Fanout);
+
+Console.WriteLine("Kuyruk adını giriniz: ");
+string queueName = Console.ReadLine();
+
+channel.QueueDeclare(
+	queue: queueName,
+	exclusive: false);
+
+channel.QueueBind(
+	queue: queueName,
+	exchange: "fanout-exchange-example",
+	routingKey: string.Empty
+	);
+EventingBasicConsumer consumer = new(channel);
+
+channel.BasicConsume(
+	queue: queueName,
+	autoAck: true,
+	consumer: consumer
+	);
+
+consumer.Received += (sender, e) =>
+{
+	string message = Encoding.UTF8.GetString(e.Body.Span);
+	Console.WriteLine(message);
+};
+
+Console.Read();
---
+++ RabbitMQ.FanoutPublisher/Program.cs
@@ -0,0 +1,26 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+
+factory.Uri = new Uri("amqps://mkckuyqi:T72UpsjX0A1wCShoziWPCM9mK5JgjpGx@woodpecker.rmq.cloudamqp.com/mkckuyqi");
+
+using IConnection connection = factory.CreateConnection(); 
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(
+	exchange: "fanout-exchange-example",
+	type: ExchangeType.Fanout);
+
+for (int i = 0; i < 100; i++)
+{
+	await Task.Delay(200);
+	byte[] message = Encoding.UTF8.GetBytes($"merhaba {i}");
+	channel.BasicPublish(
+		exchange: "fanout-exchange-example",
+		routingKey: string.Empty,
+		body: message
+		);
+}
+
+Console.Read();
