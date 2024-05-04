8b098318d13ef996df10fdeb0cc9c4bdb5b2a3b9
Onur Nafi Guzel <nafi.guzel@consultant.boyner.com.tr>
2024-05-04 19:37:54 +0300

    8.DERS

---
+++ RabbitMQ.Header.Consumer/Program.cs
@@ -0,0 +1,44 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+
+factory.Uri = new Uri("amqps://mkckuyqi:T72UpsjX0A1wCShoziWPCM9mK5JgjpGx@woodpecker.rmq.cloudamqp.com/mkckuyqi");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(
+	exchange: "header-exchange-example",
+	type: ExchangeType.Headers
+	);
+
+Console.WriteLine("Header value değerini giriniz");
+string value = Console.ReadLine();
+
+string queueName = channel.QueueDeclare().QueueName;
+
+channel.QueueBind(
+	queue: queueName,
+	exchange: "header-exchange-example",
+	routingKey: string.Empty,
+	new Dictionary<string, object>
+	{
+		["no"] = value
+	});
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(
+	queue: queueName,
+	autoAck: true,
+	consumer: consumer
+	);
+
+consumer.Received += (sender, e) =>
+{
+	string message = Encoding.UTF8.GetString(e.Body.Span);
+	Console.WriteLine(message);
+};
+
+Console.Read();
---
+++ RabbitMQ.Header.Publisher/Program.cs
@@ -0,0 +1,38 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+
+factory.Uri = new Uri("amqps://mkckuyqi:T72UpsjX0A1wCShoziWPCM9mK5JgjpGx@woodpecker.rmq.cloudamqp.com/mkckuyqi");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(
+	exchange: "header-exchange-example",
+	type: ExchangeType.Headers
+	);
+
+for (int i = 0; i < 100; i++)
+{
+	await Task.Delay(200);
+	byte[] message = Encoding.UTF8.GetBytes($"merhaba {i}");
+    Console.WriteLine("Header values değerini giriniz");
+	string value = Console.ReadLine();
+
+	IBasicProperties basicProperties =  channel.CreateBasicProperties();
+
+	basicProperties.Headers = new Dictionary<string, object>
+	{
+		["no"] = value
+	};
+
+	channel.BasicPublish(
+		exchange: "header-exchange-exampler",
+		routingKey: string.Empty,
+		body: message,
+		basicProperties: basicProperties
+		);
+}
+
+Console.Read();
